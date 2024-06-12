91ed3870ab7c9024ce602910177eab4e74459324
Werner Damboeck <werner.damboeck@htlstp.ac.at>
2024-06-12 16:39:06 +0200

    Initial Commit

---
+++ src/main/java/at/letto/lettopluginuhr/LettoPluginuhrApplication.java
@@ -0,0 +1,13 @@
+package at.letto.lettopluginuhr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LettoPluginuhrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LettoPluginuhrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/letto/lettopluginuhr/LettoPluginuhrApplicationTests.java
@@ -0,0 +1,13 @@
+package at.letto.lettopluginuhr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LettoPluginuhrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
