82c880c296a20e7cb57791105f5396745b2b0924
Werner Damboeck <werner.damboeck@letto.at>
2025-10-10 09:41:26 +0200

    App

--- src/main/java/at/letto/basespringboot/service/BaseResourcenService.java
+++ src/main/java/at/letto/basespringboot/service/BaseResourcenService.java
@@ -10,16 +10,37 @@ import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.util.Vector;
 
+/**
+ *  Service um Dateien aus den Ressourcen in das Dateisystem zu kopieren oder zu laden
+ */
+
 public class BaseResourcenService {
 
+    /** Kopiert eine Textdatei aus den Ressourcen in das Dateisystem
+     * @param resourceName   Name der Ressource (Pfad relativ zu src/main/resources)
+     * @param targetFileName Zieldatei in die kopiert werden soll
+     * @return Fehlermeldung wenn etwas nicht funktioniert hat, sonst leerer String
+     */
     public String copyTextFileFromResource(String resourceName, String targetFileName) {
         File targetFile = new File(targetFileName);
         return copyTextFileFromResource(resourceName,targetFile, "UTF-8");
     }
+
+    /** Kopiert eine Textdatei aus den Ressourcen in das Dateisystem
+     * @param resourceName   Name der Ressource (Pfad relativ zu src/main/resources)
+     * @param targetFile     Zieldatei in die kopiert werden soll
+     * @return Fehlermeldung wenn etwas nicht funktioniert hat, sonst leerer String
+     */
     public String copyTextFileFromResource(String resourceName, File targetFile) {
         return copyTextFileFromResource(resourceName, targetFile, "UTF-8");
     }
 
+    /** Kopiert eine Textdatei aus den Ressourcen in das Dateisystem
+     * @param resourceName   Name der Ressource (Pfad relativ zu src/main/resources)
+     * @param targetFile     Zieldatei in die kopiert werden soll
+     * @param encoding       Encoding der Textdatei (z.B. UTF-8, ISO-8859-1, ...)
+     * @return Fehlermeldung wenn etwas nicht funktioniert hat, sonst leerer String
+     */
     public String copyTextFileFromResource(String resourceName, File targetFile, String encoding) {
         String msg = "";
         ClassPathResource res = new ClassPathResource(resourceName);
@@ -32,11 +53,21 @@ public class BaseResourcenService {
         return msg;
     }
 
+    /** Kopiert eine Datei aus den Ressourcen in das Dateisystem
+     * @param resourceName   Name der Ressource (Pfad relativ zu src/main/resources)
+     * @param targetFileName Zieldatei in die kopiert werden soll
+     * @return Fehlermeldung wenn etwas nicht funktioniert hat, sonst leerer String
+     */
     public String copyFileFromResource(String resourceName, String targetFileName) {
         File targetFile = new File(targetFileName);
         return copyFileFromResource(resourceName,targetFile);
     }
 
+    /** Kopiert eine Datei aus den Ressourcen in das Dateisystem
+     * @param resourceName   Name der Ressource (Pfad relativ zu src/main/resources)
+     * @param targetFile     Zieldatei in die kopiert werden soll
+     * @return Fehlermeldung wenn etwas nicht funktioniert hat, sonst leerer String
+     */
     public String copyFileFromResource(String resourceName, File targetFile) {
         String msg = "";
         ClassPathResource res = new ClassPathResource(resourceName);
@@ -48,6 +79,10 @@ public class BaseResourcenService {
         return msg;
     }
 
+    /** Lädt eine Textdatei aus den Ressourcen
+     * @param resourceName   Name der Ressource (Pfad relativ zu src/main/resources)
+     * @return Vektor mit den Zeilen der Datei oder null wenn die Datei nicht geladen werden konnte
+     */
     public Vector<String> loadTextFileFromResource(String resourceName) {
         ClassPathResource res = new ClassPathResource(resourceName);
         try {
