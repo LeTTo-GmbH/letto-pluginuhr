06e8861a6bdf79da3945be43543d8c4191db02e2
Werner Damboeck <werner.damboeck@letto.at>
2026-09-03 12:14:48 +0200

    Setup 1.3 configuration

--- finish.sh
+++ finish.sh
@@ -37,8 +37,21 @@ if [[ $VERSION == "1.3" ]] ; then
   if [[ $daily == "on" || $stable == "on" || $beta == "on" ]] ; then
     echo restart daily-Version on build-server
     cd /opt/letto/docker/compose/letto
-    docker compose -f docker-service-pluginuhr.yml down
-    docker compose -f docker-service-pluginuhr.yml up -d
+
+    ymlfile="docker-service-pluginuhr.yml"
+    docker compose -f $ymlfile down
+
+    # yml-Datei anpassen !
+    downloadfile="/download/install-1.3/yml/$ymlfile"
+    if [[ -s "$downloadfile" ]]; then
+      # cp $downloadfile $ymlfile
+      ./replacetag.sh $ymlfile $downloadfile
+    else
+      echo "Error: File '$downloadfile' does not exist or is empty."
+    fi
+
+    docker compose -f $ymlfile up -d
+
   fi
 fi
 
